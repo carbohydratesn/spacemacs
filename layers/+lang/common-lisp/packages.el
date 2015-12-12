04c3b5cc619cc94ce60282c23a688a8dd24235c2
carbohydratesn <carbohydratesn@users.noreply.github.com>
2015-12-12 10:13:44 -0700

    Check for the presence of smartparens before calling its functions
    slime was not working for me today - it would crash before getting to a repl.  
    
    "error in process filter: Symbol's function definition is void: smartparens-strict-mode"
    
    and if the reference to smartparens-strict-mode in packages.el was commented out, it returned 
    "error in process filter: Symbol's function definition is void: turn-off-smartparens-mode"
    
    smartparens does not seem to be included in the common-lisp layer, for some reason.  I am using spacemacs-base with the emacs-lisp and common-lisp layers, and exwm as an additional package.
    
    Adding a check to see whether smartparens is present before calling those functions allows slime to reach a repl, even when smartparens is not installed.

--- layers/+lang/common-lisp/packages.el
+++ layers/+lang/common-lisp/packages.el
@@ -26,12 +26,14 @@
       ;; enable fuzzy matching in code buffer and SLIME REPL
       (setq slime-complete-symbol*-fancy t)
       (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
-      ;; enabel smartparen in code buffer and SLIME REPL
+      ;; enable smartparen in code buffer and SLIME REPL
       ;; (add-hook 'slime-repl-mode-hook #'smartparens-strict-mode)
       (defun slime/disable-smartparens ()
         (smartparens-strict-mode -1)
         (turn-off-smartparens-mode))
-      (add-hook 'slime-repl-mode-hook #'slime/disable-smartparens)
+      ;; only add the hook if smartparens is present
+      (when (require 'smartparens nil 'noerror)
+        (add-hook 'slime-repl-mode-hook #'slime/disable-smartparens))
       (spacemacs/add-to-hooks 'slime-mode '(lisp-mode-hook)))
     :config
     (progn
